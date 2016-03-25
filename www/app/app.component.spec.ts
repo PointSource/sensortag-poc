b9dbe4341cb802f2b1c238efbe959c20259ad5f1
Valerie Graybeal <valerie.graybeal@pointsource.com>
2016-03-25 10:44:32 -0400

    Wrote first couple of unit tests to track sensor tag behavior

--- www/app/app.component.spec.ts
+++ www/app/app.component.spec.ts
@@ -1,16 +1,44 @@
 import {NgZone} from 'angular2/core';
 import {AppComponent} from "./app.component"
 
+var sensortag;
+var tisensortag;
+
+beforeEach(() => {
+	sensortag = {
+		statusCallback: function() { return this },
+		errorCallback: function() { },
+	};
+
+	tisensortag = {
+		createInstance: function() {
+			return sensortag
+		}
+	}
+})
+
 
 describe('Appcomponent', () => {
-	let ngZone:NgZone
-
-	beforeEach(() => {
-	})
-
+	let ngZone: NgZone;
+	
 	it('initializes sensor tag', () => {
-		let appComponent = new AppComponent(ngZone);
-		expect(true).toEqual(true);
+		spyOn(tisensortag, "createInstance").and.returnValue(sensortag);
+		let appComponent = new AppComponent({
+			tisensortag: tisensortag
+		}, ngZone);
+		expect(tisensortag.createInstance).toHaveBeenCalled();
 	});
+
+
+	it('initializes callbacks on the sensor tag', () => {
+		spyOn(sensortag, "statusCallback").and.returnValue(sensortag);
+		spyOn(sensortag, "errorCallback").and.returnValue(sensortag);
+		let appComponent = new AppComponent({
+			tisensortag: tisensortag
+		}, ngZone);
+		expect(sensortag.statusCallback).toHaveBeenCalled();
+		expect(sensortag.errorCallback).toHaveBeenCalled();
+	});
+
 
 });
