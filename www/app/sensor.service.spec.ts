94adef2de9734072c56359d4d54c24a2a8eb8e02
Valerie Graybeal <valerie.graybeal@pointsource.com>
2016-04-14 10:30:44 -0400

    Fixing up sensor service tests

--- www/app/sensor.service.spec.ts
+++ www/app/sensor.service.spec.ts
@@ -1,23 +1,5 @@
 import {SensorService} from "./sensor.service"
 
-class MockSensorService extends SensorService {
-	sensors = [{
-		systemId: "1234",
-		policyNumber: "Job1",
-		name: "Garage",
-		status: "CONNECTED",
-		sensortag: {},
-		data: {},
-	},{
-		systemId: "5678",
-		policyNumber: "Job2",
-		name: "Garage2",
-		status: "CONNECTED",
-		sensortag: {},
-		data: {},
-	}];
-}
-
 import {
 	beforeEach,
 	beforeEachProviders,
@@ -27,12 +9,10 @@ import {
 	inject,
 	injectAsync } from 'angular2/testing';
 import {provide} from "angular2/core"
-import {Http} from 'angular2/http';
 
 beforeEachProviders(() => {
 	return [
-		provide(Http, { useValue: {} }),
-		provide(SensorService, {useClass: MockSensorService})
+		SensorService
 	]
 })
 
@@ -44,6 +24,24 @@ describe('Sensor Service', () =>
 		it('returns the sensors for a specific job', inject(
 			[SensorService],
 			(_sensorService: SensorService) => {
+
+				_sensorService.addSensor({
+					systemId: "1234",
+					policyNumber: "Job1",
+					name: "Garage",
+					status: "CONNECTED",
+					sensortag: {},
+					data: {},
+				});
+
+				_sensorService.addSensor({
+					systemId: "5678",
+					policyNumber: "Job2",
+					name: "Garage2",
+					status: "CONNECTED",
+					sensortag: {},
+					data: {},
+				});
 
 			let sensorsForJob = _sensorService.getSensorsForPolicy("Job1");
 			expect(sensorsForJob).toEqual([{
